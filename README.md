a8974466e98cc10d86fcc6942e5c23b8d80c3a14
iamrob <rob.duits@incentro.com>
2022-11-08 10:21:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
 ## NativeOnboarding
-[Set up A simple Native Onboarding section]
+
+Set up A simple Native Onboarding section.
 
 ## Features
-[feature highlights]
+
+A configurable set of onboarding pages.
+
+Show skip button? (`boolean`).
+
+Complete action.
+
+Skip action.
+
+Set colors for text and background elements.
 
 ## Usage
-[step by step instructions]
 
-## Demo project
-[link to sandbox]
+Generate a simple onboarding by filling the object list group settings. At least 1 'page' is required to add. A page consists of the following:
 
-## Issues, suggestions and feature requests
-[link to GitHub issues]
+- Title
+- Background color
+- Subtitle
+- Image
+
+! Keep the content of the text limited, as the contents of the onboarding is not vertical scrollable.
 
 ## Development and contribution
 
 1. Install NPM package dependencies by using: `npm install`. If you use NPM v7.x.x, which can be checked by executing `npm -v`, execute: `npm install --legacy-peer-deps`.
 1. Run `npm start` to watch for code changes. On every change:
-    - the widget will be bundled;
-    - the bundle will be included in a `dist` folder in the root directory of the project;
-    - the bundle will be included in the `deployment` and `widgets` folder of the Mendix test project.
+   - the widget will be bundled;
+   - the bundle will be included in a `dist` folder in the root directory of the project;
+   - the bundle will be included in the `deployment` and `widgets` folder of the Mendix test project.
 
 [specify contribution]
--- src/NativeOnboarding.jsx
+++ src/NativeOnboarding.jsx
@@ -1,162 +1,157 @@
-import { Dimensions, Image, View } from 'react-native';
-import { createElement, useState } from 'react';
-import FilledButton from './buttons/FilledButton';
-import Onboarding from 'react-native-onboarding-swiper';
-import TextButton from './buttons/TextButton';
-import { mergeNativeStyles } from '@mendix/pluggable-widgets-tools';
-import { spacing } from './util/spacing';
+import { Dimensions, Image, View } from "react-native";
+import { createElement, useState } from "react";
+import FilledButton from "./buttons/FilledButton";
+import Onboarding from "react-native-onboarding-swiper";
+import TextButton from "./buttons/TextButton";
+import { spacing } from "./util/spacing";
 
 // based on https://github.com/jfilter/react-native-onboarding-swiper
 
-const windowWidth = Dimensions.get('window').width;
-const defaultStyle = {
-  container: {
-    height: '100%',
-    width: '100%',
-    zIndex: 10,
-  },
-  onboarding: { justifyContent: 'center' },
-  title: {
-    color: '#4B4B4B',
-    fontSize: 30,
-    fontWeight: '400',
-    textAlign: 'left',
-    width: windowWidth - spacing.regular * 2,
-  },
-  subTitle: {
-    color: '#4B4B4B',
-    fontSize: 16,
-    lineHeight: 16 * 1.5,
-    textAlign: 'left',
-    width: windowWidth - spacing.regular * 2,
-  },
-  image: {
-    margin: 0,
-    padding: 0,
-    resizeMode: 'contain',
-    maxWidth: windowWidth - spacing.regular * 2,
-  },
-  imageContainer: {
-    paddingBottom: spacing.regular,
-    height: 'auto',
-    width: 'auto',
-  },
+const windowWidth = Dimensions.get("window").width;
+const styles = {
+    container: {
+        height: "100%",
+        width: "100%",
+        zIndex: 10
+    },
+    onboarding: { justifyContent: "center" },
+    title: {
+        color: "#4B4B4B",
+        fontSize: 30,
+        fontWeight: "400",
+        textAlign: "left",
+        width: windowWidth - spacing.regular * 2
+    },
+    subTitle: {
+        color: "#4B4B4B",
+        fontSize: 16,
+        lineHeight: 16 * 1.5,
+        textAlign: "left",
+        width: windowWidth - spacing.regular * 2
+    },
+    image: {
+        margin: 0,
+        padding: 0,
+        resizeMode: "contain",
+        maxWidth: windowWidth - spacing.regular * 2
+    },
+    imageContainer: {
+        paddingBottom: spacing.regular,
+        height: "auto",
+        width: "auto"
+    }
 };
 
 export function NativeOnboarding({
-  bottombarHighlight,
-  bottombarTextColor,
-  currentPageDotColor,
-  doneButtonColor,
-  doneButtonText,
-  dotColor,
-  doneAction,
-  doneText,
-  nextText,
-  pages,
-  showSkip,
-  skipAction,
-  skipText,
-  style,
+    bottombarHighlight,
+    bottombarTextColor,
+    currentPageDotColor,
+    doneButtonColor,
+    doneButtonText,
+    dotColor,
+    doneAction,
+    doneText,
+    nextText,
+    pages,
+    showSkip,
+    skipAction,
+    skipText
 }) {
-  const styles = mergeNativeStyles(defaultStyle, style);
-  const onboardingPages = [];
-  const [currentPage, setCurrentPage] = useState(0);
+    const onboardingPages = [];
+    const [currentPage, setCurrentPage] = useState(0);
 
-  const Square = ({ selected }) => {
-    const backgroundColor = selected ? currentPageDotColor : 'transparent';
-    const borderColor = selected ? 'transparent' : dotColor;
+    const Square = ({ selected }) => {
+        const backgroundColor = selected ? currentPageDotColor : "transparent";
+        const borderColor = selected ? "transparent" : dotColor;
+
+        return (
+            <View
+                style={{
+                    borderRadius: 20,
+                    borderWidth: 1,
+                    width: 8,
+                    height: 8,
+                    marginHorizontal: 3,
+                    backgroundColor,
+                    borderColor
+                }}
+            />
+        );
+    };
+
+    const Skip = ({ ...props }) => (
+        <TextButton
+            title={skipText}
+            textColor={bottombarTextColor}
+            marginLeft={spacing.regular}
+            fontSize={14}
+            {...props}
+        />
+    );
+
+    const Next = ({ ...props }) => (
+        <TextButton
+            title={nextText}
+            textColor={bottombarTextColor}
+            marginRight={spacing.regular}
+            fontSize={14}
+            {...props}
+        />
+    );
+
+    const Done = ({ ...props }) => (
+        <FilledButton
+            backgroundColor={doneButtonColor}
+            title={doneText}
+            textColor={doneButtonText}
+            marginRight={spacing.regular}
+            fontSize={14}
+            {...props}
+        />
+    );
+
+    function actionButton() {
+        if (doneAction && doneAction.canExecute) {
+            doneAction.execute();
+        }
+    }
+
+    function skipButton() {
+        if (skipAction && skipAction.canExecute) {
+            setCurrentPage(0);
+            skipAction.execute();
+        }
+    }
+
+    pages.map(page =>
+        onboardingPages.push({
+            backgroundColor: page.pageBackground || "transparent",
+            title: page.pageTitle || "",
+            subtitle: page.pageSubTitle || "",
+            image: <Image source={Number(page.pageImage.value)} style={styles.image} />
+        })
+    );
 
     return (
-      <View
-        style={{
-          borderRadius: 20,
-          borderWidth: 1,
-          width: 8,
-          height: 8,
-          marginHorizontal: 3,
-          backgroundColor,
-          borderColor,
-        }}
-      />
+        <View style={styles.container}>
+            <Onboarding
+                allowFontScalingText={false}
+                bottomBarHighlight={bottombarHighlight}
+                currentPage={currentPage}
+                SkipButtonComponent={Skip}
+                DotComponent={Square}
+                NextButtonComponent={Next}
+                DoneButtonComponent={Done}
+                onSkip={skipAction && skipButton}
+                onDone={doneAction && actionButton}
+                pages={onboardingPages}
+                transitionAnimationDuration={200}
+                titleStyles={styles.title}
+                imageContainerStyles={styles.imageContainer}
+                subTitleStyles={styles.subTitle}
+                containerStyles={styles.onboarding}
+                showSkip={showSkip}
+            />
+        </View>
     );
-  };
-
-  const Skip = ({ ...props }) => (
-    <TextButton
-      title={skipText}
-      textColor={bottombarTextColor}
-      marginLeft={spacing.regular}
-      fontSize={14}
-      {...props}
-    />
-  );
-
-  const Next = ({ ...props }) => (
-    <TextButton
-      title={nextText}
-      textColor={bottombarTextColor}
-      marginRight={spacing.regular}
-      fontSize={14}
-      {...props}
-    />
-  );
-
-  const Done = ({ ...props }) => (
-    <FilledButton
-      backgroundColor={doneButtonColor}
-      title={doneText}
-      textColor={doneButtonText}
-      marginRight={spacing.regular}
-      fontSize={14}
-      {...props}
-    />
-  );
-
-  function actionButton() {
-    if (doneAction && doneAction.canExecute) {
-      doneAction.execute();
-    }
-  }
-
-  function skipButton() {
-    if (skipAction && skipAction.canExecute) {
-      setCurrentPage(0);
-      skipAction.execute();
-    }
-  }
-
-  pages.map((page) =>
-    onboardingPages.push({
-      backgroundColor: page.pageBackground || 'transparent',
-      title: page.pageTitle || '',
-      subtitle: page.pageSubTitle || '',
-      image: (
-        <Image source={Number(page.pageImage.value)} style={styles.image} />
-      ),
-    })
-  );
-
-  return (
-    <View style={styles.container}>
-      <Onboarding
-        allowFontScalingText={false}
-        bottomBarHighlight={bottombarHighlight}
-        currentPage={currentPage}
-        SkipButtonComponent={Skip}
-        DotComponent={Square}
-        NextButtonComponent={Next}
-        DoneButtonComponent={Done}
-        onSkip={skipAction && skipButton}
-        onDone={doneAction && actionButton}
-        pages={onboardingPages}
-        transitionAnimationDuration={200}
-        titleStyles={styles.title}
-        imageContainerStyles={styles.imageContainer}
-        subTitleStyles={styles.subTitle}
-        containerStyles={styles.onboarding}
-        showSkip={showSkip}
-      />
-    </View>
-  );
 }
